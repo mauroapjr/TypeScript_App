cc50e785681e3f236db2499dfccc31bbcf8490a0
mauroapjr <mauroapjr@outlook.com>
2024-02-11 22:47:23 -0500

    created database connection

---
+++ db.ts
@@ -0,0 +1,26 @@
+import { Db, MongoClient } from "mongodb";
+
+const state: {db: Db | null } = { db: null };
+
+export const connect = async ( url: string, dbname: string): Promise <void> => {
+  try {
+    if (state.db){
+      return
+    }
+
+    const client = new MongoClient(url);
+    await client.connect();
+
+    state.db = client.db(dbname)
+  }  catch (err) {
+    console.error
+    }
+  
+}
+
+export const get = (): Db => {
+  if (!state.db) {
+    throw new Error ("Connection is not initialized");
+  }
+  return state.db;
+};
